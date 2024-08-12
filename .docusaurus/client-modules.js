d37bf9c9623dfaf3755152849066b78a90866d53
Ilya T <98028533+iltars@users.noreply.github.com>
2024-08-13 00:44:11 +0900

    Deploy website - based on f205ee6b6438f6b4b931a9ea8acb50b9ddbe3e95

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("D:\\Docusaurus\\my-website\\node_modules\\infima\\dist\\css\\default\\default.css"),
-  require("D:\\Docusaurus\\my-website\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages"),
-  require("D:\\Docusaurus\\my-website\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress"),
-  require("D:\\Docusaurus\\my-website\\src\\css\\custom.css"),
-];
