e4d8aab85bd5ae525a8934b51b90853c06b72aa9
Ilya T <98028533+iltars@users.noreply.github.com>
2024-08-13 01:23:02 +0900

    Deploy website - based on ad61ae5705d01b9bccae300759d576c0583d412b

--- assets/js/814f3328.3a5b5512.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkmy_website=self.webpackChunkmy_website||[]).push([[472],{5513:s=>{s.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"A fresh start","permalink":"/https://iltars.github.io/docasaurus-sandbox/blog/mdx-blog-post","unlisted":false,"date":"2024-08-01T00:00:00.000Z"}]}')}}]);
---
+++ assets/js/acecf23e.ce43add2.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmy_website=self.webpackChunkmy_website||[]).push([[903],{1912:s=>{s.exports=JSON.parse('{"blogBasePath":"/docasaurus-sandbox/blog","blogTitle":"Blog","authorsListPath":"/docasaurus-sandbox/blog/authors"}')}}]);
